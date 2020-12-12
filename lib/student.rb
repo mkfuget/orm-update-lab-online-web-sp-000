87d5fbcd77e065533cc31845a1d28dc0f4c06211
Michael Fuget <michaelfuget@gmail.com>
2020-12-12 06:08:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,14 @@ class Student
   end
   
   def save 
-    
+        sql = <<-SQL
+      INSERT INTO students (name, album) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.album)
+ 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
   end 
 end
