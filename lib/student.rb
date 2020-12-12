78920011d30ed76f1b686e373a07f6978b7f087f
Michael Fuget <michaelfuget@gmail.com>
2020-12-12 06:22:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
       SELECT * FROM students WHERE name  = ?
     SQL
  
-    row = DB[:conn].execute(sql, name)
+    row = DB[:conn].execute(sql, name)[0]
     out = new_from_db(row)
     return out
   end 
